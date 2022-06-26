43dba795d99782b293b4a9699c224bff5c543bf9
mayouyu <2548481398@qq.com>
2022-06-26 11:47:25 +0800

    路由配置完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
     'no-unused-vars': 'off',
-    'no-undef': 'off',
+    'no-undef': 'off'
   }
 }
